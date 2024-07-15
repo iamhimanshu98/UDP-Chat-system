dd8f69304649fd53a7cbb04639529d6d5068bd57
Himanshu Kumawat <135202238+iamhimanshu98@users.noreply.github.com>
2024-07-15 15:08:40 +0530

    Add files via upload
    Chat system using python

---
+++ sender.py
@@ -0,0 +1,34 @@
+# This module is the sender end for UDP communication
+
+import socket
+import datetime
+
+sender = 'user1'
+receiver = 'user2'
+receiver_ip = "127.0.0.1"
+port_no = 2525  # range 0-65353
+
+now = datetime.datetime.now()
+date = now.strftime("%d-%m-%Y")
+time = now.strftime("%I:%M %p")
+with open(sender + '.txt', 'a+' ) as file :
+        file.write('\n' + date + '\n')
+
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+receiver_address = (receiver_ip, port_no)
+
+while True :
+    message = input(f"{sender} write your message: ")
+    encrypt_message = message.encode()
+    s.sendto(encrypt_message, receiver_address)
+
+    with open(sender + '.txt', 'a+' ) as file :
+        file.write(f"{sender} ({time}): " + message + '\n')
+
+    message = s.recvfrom(100)
+    received_meassage, receiver_address = message
+    decrypted_message = received_meassage.decode()
+    print(f"message from {receiver} :", decrypted_message)
+
+    with open(sender + '.txt', 'a+' ) as file :
+        file.write(f"{receiver} ({time}): " + decrypted_message + '\n')
